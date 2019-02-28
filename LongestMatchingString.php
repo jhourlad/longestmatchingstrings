ec9032a9af3d604cb4f3d884aa8c4f8d046bb26f
Joe Estrella <agili3dotcom@gmail.com>
2019-03-01 00:10:50 +0800

    Class file + README.md

---
+++ LongestMatchingString.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Get the longest matching string between two strings
+ *
+ * Class LongestCommonString
+ */
+
+class LongestMatchingString
+{
+    protected $tracker = [];
+    protected $max = 0;
+    protected $pointer = 0;
+
+    /**
+     * Main code logic
+     * @param $str
+     * @param $str2
+     * @return bool|string|null
+     */
+
+    function handle($str, $str2)
+    {
+        foreach (str_split($str) as $index => $char) {
+            $this->tracker[$index] = [];
+
+            foreach (str_split($str2) as $index2 => $char2) {
+                $this->compareAndIncrement($char, $index, $char2, $index2);
+            }
+        }
+
+        return $this->max ? substr($str, $this->pointer - $this->max + 1, $this->max) : null;
+    }
+
+    /**
+     * Compare current string from index to pointer. Move pointer if matched;
+     * @param $char
+     * @param $index
+     * @param $char2
+     * @param $index2
+     */
+
+    private function compareAndIncrement($char, $index, $char2, $index2)
+    {
+        $this->tracker[$index][$index2] = 0;
+
+        if ($char == $char2) {
+            if ($index == 0 || $index2 == 0) {
+                $current = 1;
+            } else {
+                if (isset($this->tracker[$index - 1][$index2 - 1])) {
+                    $current = $this->tracker[$index - 1][$index2 - 1] + 1;
+                } else {
+                    $current = 0;
+                }
+            }
+
+            $this->tracker[$index][$index2] = $current;
+
+            if ($this->tracker[$index][$index2] > $this->max) {
+                $this->max = $this->tracker[$index][$index2];
+            }
+
+            if (isset($this->tracker[$index][$index2])) {
+                if( $this->tracker[$index][$index2] == $this->max ){
+                    $this->pointer = $index;
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FindLongestMatchingString-PHP
 Find the longest matching strings between two strings
+
+Usage: 
+
+`````(new LongestCommonString())->handle($string1, $string2);`````
+
+Example: 
+
+```echo (new LongestCommonString())->handle('12345abcwxyz', '45678910abclwyxtsre');```
+
+Output:
+
+```abc```
